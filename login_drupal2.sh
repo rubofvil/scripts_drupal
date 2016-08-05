c9f4a56bc9fd966b7e5249793ad01f12e22593a7
ruben <r.pineda@ixiam.com>
2016-08-05 15:48:54 +0200

    Add script to login selecting site in configuration

---
+++ login_drupal2.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+_VERSION="1.1"
+
+source config_login_drupal.sh
+
+
+select_source_server () {
+
+  _SOURCE_SITES=()
+
+  for i in "${!_SOURCE_SITES_ASSOCIATIVE[@]}"
+  do
+    _SOURCE_SITES+=($i)
+  done
+
+  select SOURCE_SITE in "${_SOURCE_SITES[@]}";do      
+    if [ "$SOURCE_SITE" = "Quit" ]; then
+      echo done
+      exit
+    else       		
+      URL="$(ssh "${_SOURCE_SITES_ASSOCIATIVE[$SOURCE_SITE]}" drush @"$SOURCE_SITE" uli)"          
+      echo "ssh "${_SOURCE_SITES_ASSOCIATIVE[$SOURCE_SITE]}" drush @"$SOURCE_SITE" uli";
+			xdg-open "${URL}"  			
+      exit
+    fi
+  done
+}
+###---------------------------------------### Main Program ##---------------------------------------##
+clear
+
+select_source_server 
+
+##ssh rpineda.ftp@rpineda.server-dev.ixiam.lan drush @alboan.server.ixiam.lan uli | xclip -selection c
